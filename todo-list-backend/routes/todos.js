5522564717b2bf62eee6896ffe9204be7eb9db0f
andrewelagin <andrewelagin@yojji.io>
2019-07-16 15:08:08 +0300

    Added get todos root. Added get todos saga

--- todo-list-backend/routes/todos.js
+++ todo-list-backend/routes/todos.js
@@ -1,33 +1,39 @@
 const express = require('express');
 const router = express.Router();
 
-var mysql      = require('mysql');
+var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host     : 'localhost',
-    database : 'testdb',
-    user     : 'yojji',
-    password : 'password',
+  host: 'localhost',
+  database: 'testdb',
+  user: 'yojji',
+  password: 'password'
 });
 
-connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting: ' + err.stack);
-        return;
-    }
-    console.log('Connected as id ' + connection.threadId);
+// connection.connect((error) => {
+//     if (error) {
+//         console.erroror('error connecting: ' + error.stack);
+//         return;
+//     }
+//     console.log('Connected as id ' + connection.threadId);
 
-});
+// });
 
 router.get('/todos', (req, res) => {
-  connection.query("SELECT * FROM categories", (err, result) => {
-    if (err) throw err;
+  connection.query('SELECT * FROM todos', (error, result) => {
+    if (error) throw error;
     console.log(result);
     res.send(result);
   });
 });
 
 router.post('/todos', (req, res) => {
-  res.send('im the about page!');
+  // console.log(req.body.json())
+  const values = req.body.json().then(todo => Object.values(todo));
+  connection.query("INSERT INTO todos(text, categoryId) VALUES ?", [values],  (error, result) => {
+    if (error) throw error;
+    console.log(result);
+    res.send('Todo has added successfully ' + req.body);
+  });
 });
 
 module.exports = router;
